70071f89a4608376e7dc30bdf06413a8cf1bbf82
Michael Chan <michael.chan@automattic.com>
2022-04-05 15:23:33 +1000

    Add filter to toggle on the SQL_NO_FOUND_ROWS change to Count as there is some evidence to suggest that SQL_CALC_FOUND_ROWS is more performant when there are JOIN statements present

--- performance/vip-tweaks.php
+++ performance/vip-tweaks.php
@@ -25,6 +25,19 @@ function wpcom_vip_enable_performance_tweaks() {
 	// Busts the month's filtering caches when needed for both attachments and posts/CPTs.
 	add_action( 'add_attachment', 'wpcom_vip_maybe_bust_available_months_cache' );
 	add_action( 'save_post', 'wpcom_vip_maybe_bust_available_months_cache' );
+
+	// This disables and replaces the SQL_CALC_FOUND_ROWS call that Core makes with a more performant `count` query
+	add_filter( 'pre_get_posts', 'wpcom_vip_set_no_found_rows', 10, 1 );
+	add_filter( 'posts_clauses', 'wpcom_vip_set_found_post_value', 10, 2 );
+}
+
+
+function wpcom_vip_is_no_found_rows_disabled() {
+	if ( ! apply_filters( 'wpcom_vip_no_found_rows_disable', true ) ) {
+		return false;
+	}
+ 
+	return true;
 }
 
 /**
@@ -32,6 +45,58 @@ function wpcom_vip_enable_performance_tweaks() {
  */
 function wpcom_vip_disable_performance_tweaks() {
 	remove_action( 'after_setup_theme', 'wpcom_vip_enable_performance_tweaks' );
+}
+
+if ( ! function_exists( 'wpcom_vip_set_no_found_rows' ) ) {
+	/**
+	 * In the WP_Query class this stops the use of SQL_CALC_FOUND_ROWS in the MySQL query it generates
+	 */
+	function wpcom_vip_set_no_found_rows( \WP_Query $wp_query ) {
+		if ( ! wpcom_vip_is_no_found_rows_disabled() ) {
+			return false;
+		}
+		
+		$wp_query->set( 'no_found_rows', true );
+	}
+}
+
+if ( ! function_exists( 'wpcom_vip_set_found_post_value' ) ) {
+	/**
+	 * Utilises query clauses to reconstruct a replacement for the SQL_CALC_FOUND_ROWS which is disabled via wpcom_vip_set_no_found_rows 
+	 * Then constructing and returning the pagination rules
+	 */
+	function wpcom_vip_set_found_post_value( $clauses, \WP_Query $wp_query ) {
+		if ( ! wpcom_vip_is_no_found_rows_disabled() ) {
+			return false;
+		}
+		// Don't proceed if it's a singular page.
+		if ( $wp_query->is_singular()  ) {
+			return $clauses;
+		}
+	
+		if ( ! wpcom_vip_is_no_found_rows_disabled() ) {
+			return false;
+		}
+
+		global $wpdb;
+	
+		// Check if any clauses are set.
+		$where = isset( $clauses[ 'where' ] ) ? $clauses[ 'where' ] : '';
+		$join = isset( $clauses[ 'join' ] ) ? $clauses[ 'join' ] : '';
+		$distinct = isset( $clauses[ 'distinct' ] ) ? $clauses[ 'distinct' ] : '';
+	
+		//Replace the SQL_CALC_FOUND_ROWS query with a count call https://dev.mysql.com/doc/refman/8.0/en/information-functions.html#function_found-rows
+		$wp_query->found_posts = $wpdb->get_var( "SELECT $distinct COUNT(*) FROM {$wpdb->posts} $join WHERE 1=1 $where" );
+	
+		// Work out how many posts per page there should be.
+		$posts_per_page = ( ! empty( $wp_query->query_vars['posts_per_page'] ) ? absint( $wp_query->query_vars['posts_per_page'] ) : absint( get_option( 'posts_per_page' ) ) );
+	
+		// Set the max_num_pages.
+		$wp_query->max_num_pages = ceil( $wp_query->found_posts / $posts_per_page );
+	
+		// Return the $clauses so the main query can run.
+		return $clauses;
+	}
 }
 
 /**
